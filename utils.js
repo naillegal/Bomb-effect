33eb41e22c800ca9d5610f54fba35352e9578072
orxan666 <orxanallahyarov03@gmail.com>
2022-10-08 11:10:23 +0400

    first commit

---
+++ utils.js
@@ -0,0 +1,3 @@
+const explosionSound = new Audio('explosion.mp3')
+const burningSound = new Audio('burning.mp3')
+const showExplosion = () => document.querySelector('.explosion').classList.add('explosion-start')
