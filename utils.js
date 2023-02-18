2266e9a3e95f28dbd341bb5ea3d83f6525a7a4d2
naillegal <quluzade-nail@mail.ru>
2023-02-18 16:13:33 +0400

    Bomb-effect

--- utils.js
+++ utils.js
@@ -1,3 +1,14 @@
 const explosionSound = new Audio('explosion.mp3')
 const burningSound = new Audio('burning.mp3')
 const showExplosion = () => document.querySelector('.explosion').classList.add('explosion-start')
+const bomba = document.querySelector('.bomb-image')
+const explosion = document.querySelector('.explesion')
+const ropeDiv = document.querySelector('.rope-div')
+bomba.addEventListener('click', function(){
+    ropeDiv.classList.add('active')
+    setTimeout(()=>{
+        showExplosion()
+        burningSound.play()
+        explosionSound.play()
+    },1000);
+})
